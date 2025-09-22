9d173f938ee93e6c802e3bcc2d23b8927e90a12e
Aadithyaa <aadiramki01@gmail.com>
2025-09-22 21:04:12 +0530

    Featured Products Update

--- backend/controlls/product.control.js
+++ backend/controlls/product.control.js
@@ -1,4 +1,5 @@
 import Product from "../models/product.model.js"
+import { redis } from "../library/redis.js"
 
 export const getAllProducts = async(req , res) =>{
     try{
@@ -9,5 +10,27 @@ export const getAllProducts = async(req , res) =>{
     catch(error){
         console.log("Error fetching all the product , please check again" , error.message)
         res.status(500).json({message:"serverError"})
+    }
+}
+
+
+export const getFeaturedProducts = async(req , res) =>{
+    try{
+        let products = await redis.get("featured_Products")
+        if (products){
+            return res.json(JSON.parse(products))
+        }
+        products = await Product.find({isFeatured: true}).lean()
+
+        if (!products){
+            return res.status(400).json({message : "There are no featured Items"})
+        }
+        await redis.set("featured_Products" , JSON.stringify(products))
+
+        res.json({products})
+    }
+    catch(error){
+        console.log("ERROR FETCHING FEATURED PRODUCTS , UNEXPECTED ERROR OCCURED")
+        res.status(500).json({message : "Server Error"})
     }
 }
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import {protectRoute , adminRoute} from "../middlewares/auth.middleware.js"
-import {getAllProducts} from "../controlls/product.control.js"
+import {getAllProducts , getFeaturedProducts} from "../controlls/product.control.js"
 
 const router = express.Router()
 
 router.get("/" , protectRoute , adminRoute , getAllProducts);
+router.get("/featured" , getFeaturedProducts)
 
 export default router;
